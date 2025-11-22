ad8217475689b924fc897f48efae1762f563581d
libole <libole@bytedance.com>
2025-11-23 01:55:45 +0800

    conv

---
+++ conv.go
@@ -0,0 +1,41 @@
+package youdao
+
+import (
+	"fmt"
+	"github.com/bytedance/gg/gslice"
+)
+
+func convYouDaoResp(youDaoResp *rawYouDaoResponse) (*TranslateResponse, error) {
+	if youDaoResp == nil {
+		return nil, fmt.Errorf("youdao response is nil")
+	}
+
+	wordData := gslice.Get(youDaoResp.Ec.Word, 0).ValueOrZero()
+
+	return &TranslateResponse{
+		Ukphone:         wordData.Ukphone,
+		Usphone:         wordData.Usphone,
+		Translations:    gslice.Map(wordData.Trs, func(tr Translation) string { return tr.Tr[0].L.I[0] }),
+		WebTranslations: convWebTranslations(youDaoResp.WebTrans.WebTranslation),
+		WordForms:       gslice.Map(wordData.Wfs, func(wf WordFrom) string { return wf.Wf.Name + wf.Wf.Value }),
+		Etymologies: gslice.Map(youDaoResp.Etym.Etyms.Zh, func(etymology EtymologyZh) *Etymology {
+			return &Etymology{Value: etymology.Value, Desc: etymology.Desc}
+		}),
+		EgSentences: gslice.Map(youDaoResp.BlngSentsPart.SentencePair, func(sentencePair EgSentencePair) *EGSentence {
+			return &EGSentence{Sentence: sentencePair.Sentence, Translation: sentencePair.SentenceTranslation}
+		}),
+	}, nil
+}
+
+func convWebTranslations(webTranslations []webTranslation) []string {
+	var results []string
+	for _, webTranslation := range webTranslations {
+		if webTranslation.Same != "true" {
+			continue
+		}
+		for _, trans := range webTranslation.Trans {
+			results = append(results, trans.Value)
+		}
+	}
+	return results
+}
--- query.go
+++ query.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/bytedance/gg/gslice"
 	"github.com/footgod368/translator-sdk/utils"
 	"github.com/sirupsen/logrus"
 	"io"
@@ -36,39 +35,4 @@ func Query(ctx context.Context, text string) (*TranslateResponse, error) {
 		return nil, err
 	}
 	return translateResponse, nil
-}
-
-func convYouDaoResp(youDaoResp *rawYouDaoResponse) (*TranslateResponse, error) {
-	if youDaoResp == nil {
-		return nil, fmt.Errorf("youdao response is nil")
-	}
-
-	wordData := gslice.Get(youDaoResp.Ec.Word, 0).ValueOrZero()
-
-	return &TranslateResponse{
-		Ukphone:         wordData.Ukphone,
-		Usphone:         wordData.Usphone,
-		Translations:    gslice.Map(wordData.Trs, func(tr Translation) string { return tr.Tr[0].L.I[0] }),
-		WebTranslations: convWebTranslations(youDaoResp.WebTrans.WebTranslation),
-		WordForms:       gslice.Map(wordData.Wfs, func(wf WordFrom) string { return wf.Wf.Name + wf.Wf.Value }),
-		Etymologies: gslice.Map(youDaoResp.Etym.Etyms.Zh, func(etymology EtymologyZh) *Etymology {
-			return &Etymology{Value: etymology.Value, Desc: etymology.Desc}
-		}),
-		EgSentences: gslice.Map(youDaoResp.BlngSentsPart.SentencePair, func(sentencePair EgSentencePair) *EGSentence {
-			return &EGSentence{Sentence: sentencePair.Sentence, Translation: sentencePair.SentenceTranslation}
-		}),
-	}, nil
-}
-
-func convWebTranslations(webTranslations []webTranslation) []string {
-	var results []string
-	for _, webTranslation := range webTranslations {
-		if webTranslation.Same != "true" {
-			continue
-		}
-		for _, trans := range webTranslation.Trans {
-			results = append(results, trans.Value)
-		}
-	}
-	return results
 }
